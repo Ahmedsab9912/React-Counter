38856b66f1f40c436c8aebaf869bf3a3d9dc55cf
Ahmed Ejaz Baig <a.ejaz@analytica-data.com>
2024-10-02 03:11:14 +0500

    RatingAdded

--- src/Components/TestComponent.jsx
+++ src/Components/TestComponent.jsx
@@ -6,15 +6,18 @@ constructor(){
     this.state = {
         likes :0,
         dislikes:0,
+        totalRating:0,
         // Now we are handing the States
         handelLikes : () => {
             this.setState ((prevState) => ({
-                likes : prevState.likes + 1
+                likes : prevState.likes + 1,
+                totalRating: prevState.totalRating + 1
             }));
         },
         handelDisLikes : () => {
             this.setState ((prevState) => ({
-                dislikes: prevState.dislikes + 1
+                dislikes: prevState.dislikes + 1,
+                totalRating: prevState.totalRating + 1
             }));
         }
     }
@@ -30,6 +33,7 @@ render () {
         <button onClick={this.state.handelDisLikes}> 
             Dislikes 👎 ({this.state.dislikes})
         </button>
+        <h1 style={{color:"red", fontSize:"50px", fontWeight:"bold", textAlign:"center"}} >Total Rating : {this.state.totalRating}</h1>
         </>
     )
 }
